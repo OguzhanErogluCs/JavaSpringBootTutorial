d58c75372f07049fde9a6be3059252dcc855b38e
OguzhanErogluCs <66421874+OguzhanErogluCs@users.noreply.github.com>
2021-02-05 18:58:33 +0300

    final

--- src/main/java/com/example/demo/dao/PersonDao.java
+++ src/main/java/com/example/demo/dao/PersonDao.java
@@ -1,4 +1,6 @@
 package com.example.demo.dao;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import com.example.demo.model.Person;
 
@@ -10,4 +12,11 @@ public interface PersonDao {
         UUID id = UUID.randomUUID();
         return insertPerson(id, person);
     }
+
+    List<Person> selectAllPeople();
+
+    Optional<Person> selectPersonById(UUID id);
+
+    int deletePersonById(UUID id);
+    int updatePersonById(UUID id, Person person);
 }
